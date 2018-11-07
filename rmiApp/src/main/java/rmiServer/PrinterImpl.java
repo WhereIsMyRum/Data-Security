4c4faf5e24e9c029c5a7cc1fffaae38416c8beb7
Jacek <jacekpelic@gmail.com>
2018-11-07 20:04:40 +0100

    Adding external dependencies as project dependencies

--- rmiApp/src/main/java/rmiServer/PrinterImpl.java
+++ rmiApp/src/main/java/rmiServer/PrinterImpl.java
@@ -92,12 +92,39 @@ public class PrinterImpl extends UnicastRemoteObject implements PrinterInterface
 		if ((realPassword == null) || !realPassword.equals(password)) {
 			throw new InvalidUserException("wrong username or password");
 		}
-		
-		
 		String tempToken = generate_token(username);
 		tokenArray.add(tempToken);
+		String token = createJWT(60000000);
 		
-		return tempToken;
+		return token;
+	}
+
+	private String createJWT(long ttlMillis) {
+
+		//The JWT signature algorithm we will be using to sign the token
+		SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+		long nowMillis = System.currentTimeMillis();
+		Date now = new Date(nowMillis);
+
+		//We will sign our JWT with our ApiKey secret
+		byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary("Some secret stuff");
+		Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+
+		//Let's set the JWT Claims
+		JwtBuilder builder = Jwts.builder()
+				.setIssuedAt(now)
+				.signWith(signatureAlgorithm, signingKey);
+
+		//if it has been specified, let's add the expiration
+		if (ttlMillis >= 0) {
+			long expMillis = nowMillis + ttlMillis;
+			Date exp = new Date(expMillis);
+			builder.setExpiration(exp);
+		}
+
+		//Builds the JWT and serializes it to a compact, URL-safe string
+		return builder.compact();
 	}
 	
 	private String generate_token(String username) throws NoSuchAlgorithmException {
